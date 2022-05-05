24785a89c1ca0a33dffacef2a457c6fa37024285
Charlie <charlie.offerman@gmail.com>
2022-05-05 18:43:16 -0400

    added search and nearby button and text placeholders

--- eatable/src/SearchBar/SearchBar.js
+++ eatable/src/SearchBar/SearchBar.js
@@ -1,14 +1,21 @@
 import React from 'react';
+import styles from './SearchBar.module.css';
 
 export function SearchBar() {
     return (
         <div>
             <div class="field has-addons">
-                <p class="control">
-                    <input class="input" type="text" placeholder="Your email"/>
+                <p className="control">
+                    <button className="button is-static is-medium">Search</button>
                 </p>
-                <p class="control">
-                    <a class="button is-static">@gmail.com</a>
+                <p className="control">
+                    <input className={`input is-medium ${styles['input-control']}`} type="text" placeholder="Burritos, Ice Cream, Sandwiches"/>
+                </p>
+                <p className="control">
+                    <button className="button is-static is-medium">Nearby</button>
+                </p>
+                <p className="control">
+                    <input className={`input is-medium ${styles['input-control']}`} type="text" placeholder="City Name"/>
                 </p>
             </div>
         </div>
